3a8aa94a2496438f549df12b24c464c74c672b20
tobia <tobias.hoeller2005@gmail.com>
2022-09-22 20:41:41 +0200

    last commit, project finished

--- projecthue1/src/EratosthenesPrimeSieve.java
+++ projecthue1/src/EratosthenesPrimeSieve.java
@@ -10,12 +10,31 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     @Override
     public boolean isPrime(int p)
     {
+        if(p==0)
+        {
+            return false;
+        }
+        for (int i = 2; i < p; i++)
+        {
+            if (p % i == 0)
+            {
+                return false;
+            }
+        }
+        if(primes.contains(p))
+        {
+            return true;
+        }
+        primes.add(p);
         return true;
     }
 
     @Override
     public void printPrimes()
     {
-
+        for(int p:primes)
+        {
+            System.out.println(p);
+        }
     }
 }
--- projecthue1/src/Main.java
+++ projecthue1/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static ArrayList<Integer> numbers;
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Obergrenze für den Primzahlenrechner:");
+        int og = Integer.parseInt(s.nextLine());
+        EratosthenesPrimeSieve ogtogive = new EratosthenesPrimeSieve(og);
+        numbers = new ArrayList<>();
+
+        for (int i = 2; i <= ogtogive.obergrenze; i++) {
+            numbers.add(i);
+        }
+        int wtrue =0;
+        while(wtrue< ogtogive.obergrenze)
+        {
+            primesieve(numbers, ogtogive);
+            wtrue++;
+        }
+        ogtogive.printPrimes();
+
+    }
+    public static void primesieve(ArrayList<Integer> list, EratosthenesPrimeSieve og) {
+        for (int i : list) {
+            if (og.isPrime(i)) {
+                int copy = i;
+                int multi = 2;
+                int t = 0;
+                while (t < og.obergrenze - 1) {
+
+                    t = copy * multi;
+                    if (t > og.obergrenze)
+                    {
+                        break;
+                    }
+                    multi++;
+                    if(numbers.contains(t))
+                    {Integer integer = t;numbers.remove(integer);
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
