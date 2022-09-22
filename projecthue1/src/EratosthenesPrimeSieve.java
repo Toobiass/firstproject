362b07c905b82376dc90ec9f574c05fb47297b2f
tobia <tobias.hoeller2005@gmail.com>
2022-09-22 19:53:25 +0200

    implement interface and all methods in EratosthenesPrimeSieve

--- projecthue1/src/EratosthenesPrimeSieve.java
+++ projecthue1/src/EratosthenesPrimeSieve.java
@@ -1,5 +1,21 @@
-public class EratosthenesPrimeSieve {
-    public static void main(String[] args) {
-        
+import java.util.ArrayList;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    int obergrenze;
+    ArrayList<Integer> primes = new ArrayList<>();
+
+    public EratosthenesPrimeSieve(int og) {
+        obergrenze = og;
+    }
+    @Override
+    public boolean isPrime(int p)
+    {
+        return true;
+    }
+
+    @Override
+    public void printPrimes()
+    {
+
     }
 }
